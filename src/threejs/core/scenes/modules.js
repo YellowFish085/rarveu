0f4092a2b8ef322b56797772face6246cd1ce7d6
Theo <theo.costantini@gmail.com>
2016-10-31 12:02:41 +0100

    Add basic SceneManager

---
+++ src/threejs/core/scenes/modules.js
@@ -0,0 +1,14 @@
+/**
+ * HOW TO USE
+ * import modules from './modules' directory with
+ * "import ModuleName from './modules/fileName';"
+ * then add 'ModuleName' to the 'modules' list.
+ */
+'use strict';
+
+var modules = [
+
+];
+
+export default modules;
+
