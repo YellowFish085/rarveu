4002aa8893e49fbd069b02a0bb985df90162ff68
Theo <theo.costantini@gmail.com>
2017-03-18 13:40:19 +0100

    Add endScene module

---
+++ src/js/threejs/core/hud/components/endScene/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import Vue from 'vue/dist/vue';
+
+require('./transition');
+
+const template = eval(`\`${require('./template.html')}\``);
+
+const endScene = Vue.extend({
+  template,
+
+  props: {
+    text: {
+      type    : String,
+      required: true,
+    },
+  },
+
+  mounted() {
+
+  }
+});
+
+export default endScene;
