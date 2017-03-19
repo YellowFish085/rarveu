82c5067e3eb4ccd73a3d1af03e2588bde1b5aad4
Theo <theo.costantini@gmail.com>
2017-03-19 18:49:54 +0100

    Remove loadingmanager from scenesController It was not used, we could have left it here but hey, some clean

--- src/js/threejs/core/scenes/modules/sceneA.js
+++ src/js/threejs/core/scenes/modules/sceneA.js
@@ -19,20 +19,16 @@ class SceneA extends EventEmitter {
   constructor() {
     super();
 
-    this.syncLoading = true;
-
     this._objects    = {};
 
     this._scene      = null;
-    this._loader     = null;
   }
 
   /**
    * Will be called when ScenesManager is initialized.
    * Init what's necessary here.
-   * loadingManager is the three.js loading manager used to track assets loading
    */
-  init(loadingManager) {
+  init() {
     this.bind();
 
     this.addEventListener();
@@ -40,7 +36,6 @@ class SceneA extends EventEmitter {
     this._objects.intersects = [];
 
     this._scene  = new THREE.Scene();
-    this._loader = new THREE.ObjectLoader(loadingManager);
 
     this.fillScene();
   }
